9a2ffe6f1d3abef74a1cfede84c106ac40d1e5b2
Sagi Kedmi <sagi@users.noreply.github.com>
2019-06-19 19:24:56 +0300

    100% test cov for globals

---
+++ src/globals.test.js
@@ -0,0 +1,31 @@
+const globals = require('./globals');
+
+describe('globals', () => {
+  test('SpansHive', () => {
+    const span1 = { a: 'b', c: 'd' };
+    const span2 = { e: 'f', g: 'h' };
+    globals.SpansHive.addSpan(span1);
+    globals.SpansHive.addSpan(span2);
+    expect(globals.SpansHive.getSpans()).toEqual([span1, span2]);
+  });
+
+  test('TracerGlobals', () => {
+    const event = { a: 'b', c: 'd' };
+    const context = { e: 'f', g: 'h' };
+    globals.TracerGlobals.setHandlerInputs({ event, context });
+    expect(globals.TracerGlobals.getHandlerInputs()).toEqual({
+      event,
+      context,
+    });
+
+    const token = 'abcdefg';
+    const edgeHost = 'zarathustra.com';
+    const switchOff = true;
+    globals.TracerGlobals.setTracerInputs({ token, edgeHost, switchOff });
+    expect(globals.TracerGlobals.getTracerInputs()).toEqual({
+      token,
+      edgeHost,
+      switchOff,
+    });
+  });
+});
