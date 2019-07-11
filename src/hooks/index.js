ef8bb18c085558498f283462eba7e13ff6469847
Sagi Kedmi <sagi@users.noreply.github.com>
2019-07-11 13:15:54 +0300

    Start hookin just before the actual userHandler is called

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,8 @@
-export default ({ enabled }) => {
-  if (enabled) {
-    require('./http').default();
+import httpHook from './http';
+import { isSwitchedOff, isAwsEnvironment } from '../utils';
+
+export default () => {
+  if (!isSwitchedOff() && isAwsEnvironment()) {
+    httpHook();
   }
 };
