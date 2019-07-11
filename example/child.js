c697d5d739ebb78abe50de2313735725944a3906
Sagi Kedmi <sagi@users.noreply.github.com>
2019-07-11 17:15:31 +0300

    rename main.js to lumigo.js

--- example/child.js
+++ example/child.js
@@ -3,7 +3,7 @@ const token = 't_a595aa58c126575c5c41';
 const edgeHost = 'kzc0w7k50d.execute-api.eu-west-1.amazonaws.com';
 const debug = true;
 
-const lumigo = require('./main')({ token, edgeHost, debug });
+const lumigo = require('./lumigo')({ token, edgeHost, debug });
 //const LumigoTracer = require('@lumigo/tracer');
 //const RellyTracer = new LumigoTracer({ token, host: edgeHost });
 
