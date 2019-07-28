31b7e2a73e7c6887f07db8c716f3078d4f1fba66
CircleCI <no-reply@build.com>
2019-07-28 18:43:01 +0300

    Fix Deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-git reset --hard
 set -e
 
 setup_git() {
@@ -11,15 +10,17 @@ setup_git() {
 push_tags() {
     git push origin master --tags
 }
-echo "Setting production ad NODE_ENV"
-export NODE_ENV=production
+
 echo "Deleting old node_modules"
 rm -rf node_modules
+rm -rf package-lock.json
 echo "Installing dependencies"
 npm i
+echo "Build tracer"
 npm run build
-rm -rf package-lock.json
 setup_git
+echo "Setting production ad NODE_ENV"
+export NODE_ENV=production
 echo "Getting latest changes from git"
 changes=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline)
 echo ${changes}
