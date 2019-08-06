4c0cc64614cc0dd2b1d606c7cca8d7931209b653
Dori Aviram <dori@lumigo.io>
2019-08-06 19:38:33 +0300

    fix(tests): test es2015 + es2016

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  collectCoverageFrom: ['**/src/**/**/**/**.js', '!./src/tools/xmlToJson.js'],
+  collectCoverageFrom: [
+    '**/src/**/**/**/**.js',
+    '!./src/tools/xmlToJson.js',
+    '!./src/testdata/functions/**/**.js',
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
