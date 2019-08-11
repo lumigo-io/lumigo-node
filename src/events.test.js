79f89109d19d1e4b583dde0c00a90484d2e1b143
saartochner <saart@lumigo.io>
2019-08-11 15:01:06 +0300

    fix: fix tests

--- src/events.test.js
+++ src/events.test.js
@@ -76,6 +76,7 @@ describe('events', () => {
     ).toEqual({
       api: 'gy415nuibc.execute-api.us-east-1.amazonaws.com',
       httpMethod: 'POST',
+      messageId: "deef4878-7910-11e6-8f14-25afc3e9ae33",
       resource: '/{proxy+}',
       stage: 'testStage',
     });
@@ -90,6 +91,7 @@ describe('events', () => {
     expect(events.getEventInfo(exampleApiGatewayEvent)).toEqual({
       api: 'gy415nuibc.execute-api.us-east-1.amazonaws.com',
       httpMethod: 'POST',
+      messageId: "deef4878-7910-11e6-8f14-25afc3e9ae33",
       resource: '/{proxy+}',
       stage: 'testStage',
       triggeredBy: 'apigw',
