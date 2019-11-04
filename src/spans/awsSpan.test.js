2f23ba4d0c3b22a760d3361da2bb5f094c64434d
Efi Merdler-Kravitz <efi@lumigo.io>
2019-11-04 12:10:08 +0200

    feat: wip

--- src/spans/awsSpan.test.js
+++ src/spans/awsSpan.test.js
@@ -119,6 +119,53 @@ describe('awsSpan', () => {
     expect(utils.isWarm()).toEqual(true);
   });
 
+  test('getFunctionSpan long event being trimmed', () => {
+    const expectedStartSpan = {
+      info: {
+        traceId: {
+          Root: '1-5cdcf03a-64a1b06067c2100c52e51ef4',
+          Parent: '28effe37598bb622',
+          Sampled: '0',
+          transactionId: '64a1b06067c2100c52e51ef4',
+        },
+        tracer: { name: '@lumigo/tracer', version: '0.0.123' },
+        logGroupName: '/aws/lambda/aws-nodejs-dev-hello',
+        logStreamName: '2019/05/16/[$LATEST]8bcc747eb4ff4897bf6eba48797c0d73',
+        messageId: 'deef4878-7910-11e6-8f14-25afc3e9ae33',
+        httpMethod: 'POST',
+        resource: '/{proxy+}',
+        stage: 'testStage',
+        api: 'gy415nuibc.execute-api.us-east-1.amazonaws.com',
+        triggeredBy: 'apigw',
+      },
+      vendor: 'AWS',
+      transactionId: '64a1b06067c2100c52e51ef4',
+      account: '985323015126',
+      memoryAllocated: '1024',
+      version: '$LATEST',
+      runtime: 'AWS_Lambda_nodejs8.10',
+      readiness: 'cold',
+      messageVersion: 2,
+      token: 'DEADBEEF',
+      region: 'us-east-1',
+      id: '6d26e3c8-60a6-4cee-8a70-f525f47a4caf_started',
+      envs:
+        '{"LAMBDA_TASK_ROOT":"/var/task","LAMBDA_RUNTIME_DIR":"/var/runtime","AWS_REGION":"us-east-1","AWS_DEFAULT_REGION":"us-east-1","AWS_LAMBDA_LOG_GROUP_NAME":"/aws/lambda/aws-nodejs-dev-hello","AWS_LAMBDA_LOG_STREAM_NAME":"2019/05/16/[$LATEST]8bcc747eb4ff4897bf6eba48797c0d73","AWS_LAMBDA_FUNCTION_NAME":"aws-nodejs-dev-hello","AWS_LAMBDA_FUNCTION_MEMORY_SIZE":"1024","AWS_LAMBDA_FUNCTION_VERSION":"$LATEST","_AWS_XRAY_DAEMON_ADDRESS":"169.254.79.2","_AWS_XRAY_DAEMON_PORT":"2000","AWS_XRAY_DAEMON_ADDRESS":"169.254.79.2:2000","AWS_XRAY_CONTEXT_MISSING":"LOG_ERROR","_X_AMZN_TRACE_ID":"Root=1-5cdcf03a-64a1b06067c2100c52e51ef4;Parent=28effe37598bb622;Sampled=0","AWS_EXECUTION_ENV":"AWS_Lambda_nodejs8.10","MAX_EVENT_ENTITY_SIZE":"10","LUMIGO_IS_WARM":"TRUE"}',
+      name: 'w00t',
+      type: 'function',
+      ended: 895093200000,
+      event: '{"resource',
+      started: 895093200000,
+      maxFinishTime: 895093323456,
+    };
+
+    const oldEnv = Object.assign({}, process.env);
+    process.env = { ...oldEnv, MAX_EVENT_ENTITY_SIZE: '10' };
+    expect(awsSpan.getFunctionSpan()).toEqual(expectedStartSpan);
+
+    process.env = { ...oldEnv };
+  });
+
   test('getFunctionSpan', () => {
     const expectedStartSpan = {
       info: {
