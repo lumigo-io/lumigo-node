fb71ce220525b57d9d7e7512b0f8e613000cc42c
saartochner <47108628+saartochner@users.noreply.github.com>
2020-12-28 10:57:27 +0200

    fix: auto instrument handler with absolute path
    * fix: auto instrument handler with absolute path

--- auto-instrument-handler/index.test.js
+++ auto-instrument-handler/index.test.js
@@ -35,4 +35,15 @@ describe('tracer', () => {
     ] = `../../${__dirname}/testdata/example_handler.my_handler`;
     expect(index.handler({}, {})).resolves.toEqual({ hello: 'world' });
   });
+
+  test('parseOriginalHandler relative path', () => {
+    const result = index._utils.parseOriginalHandler('src/handler.handler');
+    expect(result).toEqual(['/var/task/src/handler', 'handler']);
+  });
+
+  test('parseOriginalHandler absolute path', () => {
+    const result = index._utils.parseOriginalHandler('/opt/nodes/node_modules/handler.handler');
+    expect(result).toEqual(['/opt/nodes/node_modules/handler', 'handler']);
+  });
+
 });
