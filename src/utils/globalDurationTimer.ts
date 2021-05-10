016a5d1654266497eb24bae656bbb426fe4ea8a9
Dori Aviram <dori@lumigo.io>
2021-05-10 15:22:31 +0300

    fix: fix text

--- src/utils/globalDurationTimer.ts
+++ src/utils/globalDurationTimer.ts
@@ -3,7 +3,7 @@ import { getTracerMaxDurationTimeout } from '../utils';
 import { runOneTimeWrapper } from './functionUtils';
 
 const warnTimeoutOnce = runOneTimeWrapper(() => {
-  logger.warnClient('Lumigo tracer reached its timeout and will no longer collect data');
+  logger.warnClient('Lumigo tracer timed out and is no longer collecting data on the invocation.');
 }, {});
 
 export const GlobalDurationTimer = (() => {
