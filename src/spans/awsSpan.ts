b2c1df9cfb1440fb9851c58722c70d936f43a402
Orr Levinger <orr@lumigo.io>
2021-10-20 17:27:59 +0300

    feat: better error logs

--- src/spans/awsSpan.ts
+++ src/spans/awsSpan.ts
@@ -280,6 +280,7 @@ export const getHttpSpan = (
     }
   } catch (e) {
     logger.warn('Failed to parse aws service data', e);
+    logger.warn('The request and response are', {requestData, responseData});
   }
   // @ts-ignore
   const { awsServiceData, spanId } = serviceData;
