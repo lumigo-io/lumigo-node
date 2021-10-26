0ab048c153fa631cf37d1bd02c5a4d196eb47a0c
Orr Levinger <orr@lumigo.io>
2021-10-26 16:51:30 +0300

    feat: added support for ioredis

--- src/hooks/redis.test.js
+++ src/hooks/redis.test.js
@@ -42,8 +42,8 @@ describe('redis', () => {
 
   test('hook ioredis -> simple flow', async () => {
     const connectionOptions = 'tracer-test-cluster.1meza6.ng.0001.usw1.cache.amazonaws.com';
-    const redisClient = new Redis(connectionOptions);
     hookRedis(Redis);
+    const redisClient = new Redis(connectionOptions);
 
     await redisClient.set('Key', 'Value');
 
@@ -58,6 +58,30 @@ describe('redis', () => {
       .withResponse(`"OK"`)
       .build();
     expect(spans).toEqual([expectedSpan]);
+  });
+
+  test('hook ioredis -> rejects', async () => {
+    const connectionOptions = 'tracer-test-cluster.1meza6.ng.0001.usw1.cache.amazonaws.com';
+    hookRedis(Redis);
+    const redisClient = new Redis(connectionOptions, {
+      shouldFail: true,
+    });
+
+    try {
+      await redisClient.set('Key', 'Value');
+    } catch (e) {
+      const spans = SpansContainer.getSpans();
+      const expectedSpan = new RedisSpanBuilder()
+        .withId(spans[0].id)
+        .withStarted(spans[0].started)
+        .withEnded(spans[0].ended)
+        .withError("\"Bad data\"")
+        .withConnectionOptions(connectionOptions)
+        .withRequestCommand('set')
+        .withRequestArgs('["Key","Value"]')
+        .build();
+      expect(spans).toEqual([expectedSpan]);
+    }
   });
 
   test('hook redis -> not ready', async () => {
