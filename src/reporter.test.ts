6d73adb62b27c1365f7053f95b1297cd1a773b06
OrrLumigo <81093776+OrrLumigo@users.noreply.github.com>
2021-10-26 09:35:01 +0300

    feat: better error logs (#320)
    * feat: better error logs

--- src/reporter.test.ts
+++ src/reporter.test.ts
@@ -596,15 +596,12 @@ describe('reporter', () => {
     //Test that no error is raised
 
     const logs = ConsoleWritesForTesting.getLogs();
-    expect(logs).toEqual([
-      {
-        msg: '#LUMIGO# - WARNING - "Error in Lumigo tracer"',
-        obj: '"resultSpans.forEach is not a function"',
-      },
-      {
-        msg: '#LUMIGO# - WARNING - "Error in Lumigo tracer"',
-        obj: '"spans.map is not a function"',
-      },
-    ]);
+    expect(logs.length).toEqual(2);
+    expect(logs[0].msg).toEqual('#LUMIGO# - WARNING - "Error in Lumigo tracer"');
+    expect(logs[1].msg).toEqual('#LUMIGO# - WARNING - "Error in Lumigo tracer"');
+    expect(JSON.parse(logs[0].obj).message).toEqual('resultSpans.forEach is not a function');
+    expect(JSON.parse(logs[1].obj).message).toEqual('spans.map is not a function');
+    expect(JSON.parse(logs[0].obj).stack).toBeTruthy();
+    expect(JSON.parse(logs[1].obj).stack).toBeTruthy();
   });
 });
