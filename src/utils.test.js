5d381030b946ddf73bc650b525fbc92560e02935
CircleCI <no-reply@build.com>
2021-11-29 12:56:36 +0200

    fix: min timeout should be DEFAULT_CONNECTION_TIMEOUT + 50

--- src/utils.test.js
+++ src/utils.test.js
@@ -490,7 +490,7 @@ describe('utils', () => {
     expect(utils.getTracerMaxDurationTimeout()).toEqual(500);
   });
 
-  test('getTracerMaxDurationTimeout -> max(20%,350) of the run time', () => {
+  test('getTracerMaxDurationTimeout -> max(20%,500) of the run time', () => {
     const inputs = new HandlerInputesBuilder().withTimeout(1000).build();
     TracerGlobals.setHandlerInputs(inputs);
     expect(utils.getTracerMaxDurationTimeout()).toEqual(500);
