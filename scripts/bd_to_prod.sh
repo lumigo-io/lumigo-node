a331a6b68e8e6c68c08f33780c71dfbacd82317c
Dori Aviram <dori@lumigo.io>
2022-02-06 16:44:00 +0200

    Revert "fix: deploy (#355)"
    This reverts commit 5e6e1d96e339cfbc0b26136e0aca13f8519a35fc.

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -9,7 +9,6 @@ setup_git() {
 
 echo "Update nodejs"
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
-sudo apt-get install -y nodejs
 
 echo "Install a project with a clean state"
 npm ci
