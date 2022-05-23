30208044930b665eaadcf5bde65f64405676b631
Naor Peled <me@naor.dev>
2022-05-23 12:59:37 +0300

    wip

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,13 +1,15 @@
 import {
   isStepFunction,
-  recursiveGetKey,
-  STEP_FUNCTION_UID_KEY,
   LUMIGO_EVENT_KEY,
   md5Hash,
+  recursiveGetKey,
   safeExecute,
+  STEP_FUNCTION_UID_KEY,
 } from '../utils';
+import { EventTrigger } from './types';
+import { EventBridgeEvent } from 'aws-lambda';
 
-export const getTriggeredBy = (event) => {
+export const getTriggeredBy = (event): EventTrigger => {
   const canDetectTriggerSourceFromEventRecords =
     event?.['Records']?.[0]?.['eventSource'] || event?.['Records']?.[0]?.['EventSource'];
 
@@ -16,22 +18,23 @@ export const getTriggeredBy = (event) => {
   }
 
   if (isApiGatewayEvent(event)) {
-    return 'apigw';
+    return EventTrigger.ApiGateway;
   }
 
   if (isAppSyncEvent(event)) {
-    return 'appsync';
+    return EventTrigger.AppSync;
   }
 
   if (isStepFunction() && event && !!recursiveGetKey(event, LUMIGO_EVENT_KEY)) {
-    return 'stepFunction';
+    return EventTrigger.StepFunction;
   }
 
   if (isEventBridgeEvent(event)) {
-    return 'eventBridge';
+    return EventTrigger.EventBridge;
   }
 
-  return 'invocation';
+  // TODO: maybe rename to unknown?
+  return EventTrigger.Invocation;
 };
 
 const extractEventSourceFromRecord = (eventRecord) => {
@@ -55,21 +58,12 @@ export const isApiGatewayEvent = (event) => {
 
 export const isAppSyncEvent = (event) => {
   return (
-    (event &&
-      event['context'] &&
-      event['context']['request'] &&
-      event['context']['request']['headers'] &&
-      event['context']['request']['headers']['host'] &&
-      event['context']['request']['headers']['host'].includes('appsync-api')) ||
-    (event &&
-      event['request'] &&
-      event['request']['headers'] &&
-      event['request']['headers']['host'] &&
-      event['request']['headers']['host'].includes('appsync-api'))
+    (event?.['context']?.['request']?.['headers']?.['host']?.includes('appsync-api')) ||
+    (event?.['request']?.['headers']?.['host']?.includes('appsync-api'))
   );
 };
 
-export const isEventBridgeEvent = (event) => {
+export const isEventBridgeEvent = (event): event is EventBridgeEvent<any, any> => {
   return (
     typeof event !== undefined &&
     typeof event.version === 'string' &&
@@ -89,6 +83,7 @@ const getApiGatewayV1Data = (event) => {
 
   const api = headers['Host'] || null;
   const messageId = requestContext['requestId'];
+
   return { messageId, httpMethod, resource, stage, api };
 };
 
@@ -103,40 +98,48 @@ const getApiGatewayV2Data = (event) => {
 };
 
 export const getApiGatewayData = (event) => {
-  const version = event['version'];
-  if (version && version === '2.0') {
+  const version = event?.['version'];
+
+  if (version === '2.0') {
     return getApiGatewayV2Data(event);
   }
+
   return getApiGatewayV1Data(event);
 };
 
 export const getAppSyncData = (event) => {
   if (event.context) {
     const { host, 'x-amzn-trace-id': traceId } = event.context.request.headers;
+
     return { api: host, messageId: traceId.split('=')[1] };
   } else {
     const { host, 'x-amzn-trace-id': traceId } = event.request.headers;
+
     return { api: host, messageId: traceId.split('=')[1] };
   }
 };
 
 export const getSnsData = (event) => {
   const { TopicArn: arn, MessageId: messageId } = event.Records[0].Sns;
+
   return { arn, messageId };
 };
 
 export const getKinesisData = (event) => {
   const arn = event.Records[0].eventSourceARN;
   const messageIds = (event.Records || [])
-    .map((r) => (r['kinesis'] || {})['sequenceNumber'])
-    .filter((x) => !!x);
+    .map((record) => record?.['kinesis']?.['sequenceNumber'])
+    .filter((recordSequenceNumber) => recordSequenceNumber != null);
+
   return { arn, messageIds };
 };
 
 export const getSqsData = (event) => {
   const arn = event.Records[0].eventSourceARN;
   const messageIds = (event.Records || []).map((r) => r['messageId']).filter((x) => !!x);
+
   if (messageIds.length === 1) return { arn, messageId: messageIds[0] };
+
   return { arn, messageIds };
 };
 
@@ -147,37 +150,37 @@ export const getDynamodbData = (event) => {
     .map((record) => {
       if (
         ['MODIFY', 'REMOVE'].includes(record.eventName) &&
-        record.dynamodb &&
-        record.dynamodb.Keys
+        record?.dynamodb?.Keys
       ) {
         return md5Hash(record.dynamodb.Keys);
       } else if (record.eventName === 'INSERT' && record.dynamodb && record.dynamodb.NewImage) {
         return md5Hash(record.dynamodb.NewImage);
       }
     })
-    .filter((x) => !!x);
+    .filter((hashedRecordContent) => hashedRecordContent!= null);
+
   return { arn, messageIds, approxEventCreationTime };
 };
 
-export const getRelevantEventData = (triggeredBy, event) => {
+export const getRelevantEventData = (triggeredBy: EventTrigger, event) => {
   switch (triggeredBy) {
-    case 'sqs':
+    case EventTrigger.SQS:
       return getSqsData(event);
-    case 'dynamodb':
+    case EventTrigger.DynamoDB:
       return getDynamodbData(event);
-    case 'kinesis':
+    case EventTrigger.Kinesis:
       return getKinesisData(event);
-    case 'sns':
+    case EventTrigger.SNS:
       return getSnsData(event);
-    case 's3':
+    case EventTrigger.S3:
       return { arn: event.Records[0].s3.bucket.arn };
-    case 'apigw':
+    case EventTrigger.ApiGateway:
       return getApiGatewayData(event);
-    case 'eventBridge':
+    case EventTrigger.EventBridge:
       return { messageId: event.id };
-    case 'appsync':
+    case EventTrigger.AppSync:
       return getAppSyncData(event);
-    case 'stepFunction':
+    case EventTrigger.StepFunction:
       return {
         messageId: recursiveGetKey(event, LUMIGO_EVENT_KEY)[STEP_FUNCTION_UID_KEY],
       };
@@ -190,5 +193,6 @@ export const getRelevantEventData = (triggeredBy, event) => {
 export const getEventInfo = (event) => {
   const triggeredBy = getTriggeredBy(event);
   const eventData = safeExecute(() => getRelevantEventData(triggeredBy, event))() || {};
+
   return { ...eventData, triggeredBy };
 };
---
+++ src/events/types.ts
@@ -0,0 +1,12 @@
+export enum EventTrigger {
+  ApiGateway = 'apigw',
+  AppSync = 'appsync',
+  StepFunction = 'stepFunction',
+  EventBridge = 'eventBridge',
+  Invocation = 'invocation',
+  SQS = 'sqs',
+  DynamoDB = 'dynamodb',
+  Kinesis = 'kinesis',
+  SNS = 'sns',
+  S3 = 's3',
+}
