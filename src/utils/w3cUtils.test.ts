d019f1fa2732475b1e03c73a9b891404447cf223
CircleCI <no-reply@build.com>
2022-08-31 17:48:53 +0300

    feat: add w3c headers to http request

--- src/utils/w3cUtils.test.ts
+++ src/utils/w3cUtils.test.ts
@@ -1,5 +1,10 @@
 import { Utf8Utils } from './utf8Utils';
-import { addW3CTracePropagator, TRACEPARENT_HEADER_NAME, TRACESTATE_HEADER_NAME } from './w3cUtils';
+import {
+  addW3CTracePropagator,
+  getW3CMessageId,
+  TRACEPARENT_HEADER_NAME,
+  TRACESTATE_HEADER_NAME,
+} from './w3cUtils';
 import * as awsSpan from '../spans/awsSpan';
 
 describe('Utf8Utils', () => {
@@ -50,5 +55,17 @@ describe('Utf8Utils', () => {
     expect(parts[1]).toEqual('11111111111111222222222200000000');
     expect(parts[3]).toEqual('01');
     expect(headers[TRACESTATE_HEADER_NAME]).toEqual(`lumigo=${parts[2]}`);
+  });
+
+  test('getW3CMessageId -> happy flow', () => {
+    const headers = {
+      [TRACEPARENT_HEADER_NAME]: '00-11111111111111111111111100000000-aaaaaaaaaaaaaaaa-01',
+    };
+    expect(getW3CMessageId(headers)).toEqual('aaaaaaaaaaaaaaaa');
+  });
+
+  test('getW3CMessageId -> malformed header', () => {
+    const headers = { [TRACEPARENT_HEADER_NAME]: 'something else-aaaaaaaaaaaaaaaa-01' };
+    expect(getW3CMessageId(headers)).toBeNull();
   });
 });
