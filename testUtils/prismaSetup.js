e436435af08b3e70eb3f5330d31aa4be746e3753
Harel Moshe <harelm@lumigo.io>
2023-12-27 15:49:40 +0200

    fix(test): robust path resolution

--- testUtils/prismaSetup.js
+++ testUtils/prismaSetup.js
@@ -1,8 +1,11 @@
 import { execSync } from 'child_process'
+import path from 'path'
+
+const withPrismaFixturesPath = (...paths) => path.resolve(__dirname, 'prisma', ...paths)
 
 export default () => {
   // Clear previous test database and migrations used by Prisma
-  execSync('rm -rf ./testUtils/prisma/migrations && rm -rf ./testUtils/prisma/dev.db')
+  execSync(`rm -rf ${withPrismaFixturesPath('migrations')} && rm -rf ${withPrismaFixturesPath('dev.db')}`)
   // Create a new database to be used by the Prisma tests
-  execSync('npx prisma migrate dev --name init --schema ./testUtils/prisma/schema.prisma')
+  execSync(`npx prisma migrate dev --name init --schema ${withPrismaFixturesPath('schema.prisma')}`)
 }
