f215b46b200cd44f31a441299cd0f1920304cb55
Sagiv Oulu <sagivo@lumigo.io>
2024-05-12 17:07:22 +0300

    chore: remove testing commit

--- src/utils/requireUtils.js
+++ src/utils/requireUtils.js
@@ -1,7 +1,6 @@
 import * as logger from '../logger';
 
 export const safeRequire = (libId) => {
-  // Dummy comment, will be removed
   try {
     const customReq =
       // eslint-disable-next-line no-undef,camelcase
