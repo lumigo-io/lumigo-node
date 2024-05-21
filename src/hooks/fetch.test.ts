99570824069bb476ff85caea6a5017b810b49540
Sagiv Oulu <sagivo@lumigo.io>
2024-05-21 14:33:28 +0300

    test: ut fetch using node 18

---
+++ src/hooks/fetch.test.ts
@@ -0,0 +1,8 @@
+describe('fetch', () => {
+  test('beforeFetch', async () => {
+    // @ts-ignore
+    const response = await fetch('https://example.com/');
+
+    expect(response.status).toBe(200);
+  });
+});
