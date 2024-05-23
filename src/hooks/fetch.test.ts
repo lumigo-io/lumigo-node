76d860ac7c3fe9738e668e856f768ee369932491
Sagiv Oulu <sagivo@lumigo.io>
2024-05-23 14:28:52 +0300

    ci: run only node 18 tests on node 18

--- src/hooks/fetch.test.ts
+++ src/hooks/fetch.test.ts
@@ -2,13 +2,18 @@
  * @group node18
  */
 const [nodeVersionMajor] = process.versions.node.split('.').map(Number);
-if (nodeVersionMajor >= 18) {
-  describe('fetch', () => {
-    test('beforeFetch', async () => {
-      // @ts-ignore
-      const response = await fetch('https://example.com/');
-
-      expect(response.status).toBe(200);
+describe('fetch', () => {
+  if (nodeVersionMajor < 18) {
+    test('skip suite', () => {
+      expect(true).toBe(true);
     });
+    return;
+  }
+
+  test('beforeFetch', async () => {
+    // @ts-ignore
+    const response = await fetch('https://example.com/');
+
+    expect(response.status).toBe(200);
   });
-}
+});
