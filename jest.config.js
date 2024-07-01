3470d4821f2ee7dd32fe65ebb6a1ee7b6f99aefc
Sagiv Oulu <sagivo@lumigo.io>
2024-07-01 17:01:50 +0300

    ci: ignore fetch coverage when running on node 14

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,29 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: [
+module.exports = () => {
+  const [NODE_MAJOR_VERSION] = process.versions.node.split('.').map(Number);
+  let collectCoverageFrom = [
     '**/src/**/**/**/**.js',
     '**/src/**/**/**/**.ts',
     '!./src/tools/xmlToJson.js',
     '!./src/testdata/functions/**/**.js',
     '!./d_example/**/**',
-  ],
-  coverageDirectory: './coverage/',
-  coverageThreshold: {
-    global: {
-      lines: 99.7,
+  ];
+  if (NODE_MAJOR_VERSION < 18) {
+    collectCoverageFrom.push('!./src/hooks/fetch.ts');
+  }
+  return {
+    collectCoverage: true,
+    collectCoverageFrom: collectCoverageFrom,
+    coverageDirectory: './coverage/',
+    coverageThreshold: {
+      global: {
+        lines: 99.7,
+      },
     },
-  },
-  modulePaths: ['<rootDir>/dist'],
-  roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: ['./testUtils/jest.setup.js'],
-  globalSetup: './testUtils/prismaSetup.js',
-  silent: true,
-  watchPathIgnorePatterns: ['globalConfig'],
+    modulePaths: ['<rootDir>/dist'],
+    roots: ['<rootDir>/src'],
+    setupFilesAfterEnv: ['./testUtils/jest.setup.js'],
+    globalSetup: './testUtils/prismaSetup.js',
+    silent: true,
+    watchPathIgnorePatterns: ['globalConfig'],
+  };
 };
