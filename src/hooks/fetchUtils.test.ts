23d351fdd0ab908d0fb9395a4adc3efe58be4182
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-09-12 18:26:15 +0200

    feat(): ci debug,

---
+++ src/hooks/fetchUtils.test.ts
@@ -0,0 +1,178 @@
+import { FetchInstrumentation } from './fetch';
+
+describe('fetch', () => {
+  test.each([
+    [
+      {
+        input: 'https://example.com',
+        init: undefined,
+        expectedUrl: 'https://example.com',
+        expectedOptions: {
+          method: 'GET',
+          headers: {},
+        },
+      },
+    ],
+    [
+      {
+        input: new URL('https://example.com'),
+        init: undefined,
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'GET',
+          headers: {},
+        },
+      },
+    ],
+    [
+      {
+        input: new URL('https://example.com/'),
+        init: undefined,
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'GET',
+          headers: {},
+        },
+      },
+    ],
+    [
+      {
+        // @ts-ignore
+        input: new Request('https://example.com'),
+        init: undefined,
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'GET',
+          headers: {},
+        },
+      },
+    ],
+    [
+      {
+        // @ts-ignore
+        input: new Request(new URL('https://example.com')),
+        init: undefined,
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'GET',
+          headers: {},
+        },
+      },
+    ],
+    [
+      {
+        // @ts-ignore
+        input: new Request('https://example.com', {
+          method: 'POST',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify({ data: '12345' }),
+        }),
+        init: undefined,
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'POST',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify({ data: '12345' }),
+        },
+      },
+    ],
+    // Here we will add the init object, making sure it overrides the input values
+    [
+      {
+        // @ts-ignore
+        input: new Request('https://example.com', {
+          method: 'POST',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify({ data: '12345' }),
+        }),
+        init: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: JSON.stringify({ data: '54321' }),
+        },
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: JSON.stringify({ data: '54321' }),
+        },
+      },
+    ],
+    [
+      {
+        input: 'https://example.com',
+        init: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: JSON.stringify({ data: '54321' }),
+        },
+        expectedUrl: 'https://example.com',
+        expectedOptions: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: JSON.stringify({ data: '54321' }),
+        },
+      },
+    ],
+    [
+      {
+        input: new URL('https://example.com'),
+        init: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: JSON.stringify({ data: '54321' }),
+        },
+        expectedUrl: 'https://example.com/',
+        expectedOptions: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: JSON.stringify({ data: '54321' }),
+        },
+      },
+    ],
+    // Test different formats for body
+    [
+      {
+        input: 'https://example.com',
+        init: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          // @ts-ignore
+          body: new Blob(['Blob contents']),
+        },
+        expectedUrl: 'https://example.com',
+        expectedOptions: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          body: 'Blob contents',
+        },
+      },
+    ],
+    [
+      {
+        input: 'https://example.com',
+        init: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+          // Unsupported body type
+          body: 123,
+        },
+        expectedUrl: 'https://example.com',
+        expectedOptions: {
+          method: 'PUT',
+          headers: { 'content-type': 'application/xml' },
+        },
+      },
+    ],
+    // TODO: Test FormData body
+    // TODO: Test ReadableStream body
+  ])(
+    'Test parsing fetch command arguments: %p',
+    async ({ input, init, expectedUrl, expectedOptions }) => {
+      // @ts-ignore
+      const { url, options } = await FetchInstrumentation.parseRequestArguments({ input, init });
+      expect(url).toEqual(expectedUrl);
+      expect(options).toEqual(expectedOptions);
+    }
+  );
+});
