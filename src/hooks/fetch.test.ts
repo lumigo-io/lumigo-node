636099976cdab1e87a71870d77f867b05692a07e
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-09-16 13:35:21 +0200

    feat(): jest --forceExit,

--- src/hooks/fetch.test.ts
+++ src/hooks/fetch.test.ts
@@ -3,6 +3,11 @@ import fetchMock from 'jest-fetch-mock';
 import { FetchInstrumentation } from './fetch';
 import { SpansContainer } from '../globals';
 
+
+// Note: With the latest version of axios, using jest-fetch-mock can cause some tests to fail.
+// To work around this issue, we've moved those specific tests to a separate file (fetchUtils.test.ts).
+// This ensures that fetchMock.enableMocks() does not interfere with other tests.
+
 fetchMock.enableMocks();
 
 describe('fetch', () => {
