e494f436edcb2ca95fe3a23b904a31cd5896ff39
Harel Moshe <harelm@lumigo.io>
2025-04-23 13:06:36 +0300

    docs: add support for Node 22.x, drop support for < 16.x (#540)

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -39,7 +39,7 @@ echo "Creating layer file again so the tarball will have the right version numbe
 ./scripts/prepare_layer_files.sh
 
 echo "Creating lumigo-node layer"
-../utils/common_bash/create_layer.sh --layer-name lumigo-node-tracer --region ALL --package-folder "nodejs lumigo_wrapper" --version $(git describe --abbrev=0 --tags) --runtimes "nodejs10.x nodejs12.x nodejs14.x nodejs16.x nodejs18.x nodejs20.x"
+../utils/common_bash/create_layer.sh --layer-name lumigo-node-tracer --region ALL --package-folder "nodejs lumigo_wrapper" --version $(git describe --abbrev=0 --tags) --runtimes "nodejs16.x nodejs18.x nodejs20.x nodejs22.x"
 
 echo "Creating layer latest version arn table md file (LAYERS.md)"
 cd ../larn && npm i -g
@@ -48,16 +48,17 @@ larn -r nodejs14.x -n layers/LAYERS14x --filter lumigo-node-tracer -p ~/lumigo-n
 larn -r nodejs16.x -n layers/LAYERS16x --filter lumigo-node-tracer -p ~/lumigo-node
 larn -r nodejs18.x -n layers/LAYERS18x --filter lumigo-node-tracer -p ~/lumigo-node
 larn -r nodejs20.x -n layers/LAYERS20x --filter lumigo-node-tracer -p ~/lumigo-node
+larn -r nodejs22.x -n layers/LAYERS22x --filter lumigo-node-tracer -p ~/lumigo-node
 cd ../lumigo-node
 git add layers/LAYERS12x.md
 git add layers/LAYERS14x.md
 git add layers/LAYERS16x.md
 git add layers/LAYERS18x.md
 git add layers/LAYERS20x.md
+git add layers/LAYERS22x.md
 git commit -m "docs: layers md [skip ci]"
 
 source ../utils/common_bash/functions.sh
 send_metric_to_logz_io type=\"Release\"
 
 git push origin master
-
